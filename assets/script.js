b650d102a703b1ffbae7af1ebd0972b42760a7e4
Joshua <120212106+jminor90@users.noreply.github.com>
2022-12-10 13:04:46 -0700

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,4 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-<<<<<<< HEAD
 var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
 
@@ -26,23 +25,3 @@ console.log('Here are the topics we learned through Prework:');
 listTopics()
 console.log('Which topic should we study first?');
 selectTopic()
-=======
-
-for (var x = 0; x < topics.length; x++) {
- console.log(topics[x]);
-}
-
-/*
-if (topic === 'HTML') {
- console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
- console.log("Let's study CSS!");
-} else if (topic === 'Git') {
- console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
- console.log("Let's study JavaScript!");
-} else {
- console.log('Please try again!');
-}
-*/
->>>>>>> 888a20cfe0b9be735152e67753b6338204af5ec9
